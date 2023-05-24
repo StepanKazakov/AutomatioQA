d0c5d414bced46125b874cb37be387fa3445c11a
Stepan Kazakov <s.kazakov.1979@gmail.com>
2023-05-25 00:46:11 +0700

    check login test website

--- Selenium/lesson.py
+++ Selenium/lesson.py
@@ -4,18 +4,36 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 
 browser = webdriver.Chrome(service=ChromeService(executable_path="chromium.chromedriver"))
+base_url = 'https://www.saucedemo.com'
+login = 'standard_user'
+password_one = 'secret_sauce'
 
-browser.get('https://www.saucedemo.com')
+
+browser.get(base_url)
 browser.maximize_window()
 
+# findelement by id and fill name
 user_name = browser.find_element(by=By.ID, value="user-name")
-user_name.send_keys('standard_user')
+user_name.send_keys(login)
 
-password = browser.find_element(by=By.ID, value='password')
-password.send_keys('secret_sauce')
+# findelement by name and fill password
+password = browser.find_element(by=By.NAME, value='password')
+password.send_keys(password_one)
 
-login = browser.find_element(by=By.ID, value='login-button')
-login.click()
+# find login button by Xpath and push the button
+button_login = browser.find_element(by=By.XPATH, value='//*[@id="login-button"]')
+button_login.click()
+
+# check web page by title text
+text_products = browser.find_element(by=By.XPATH, value='//span[@class="title"]')
+value_text_products = text_products.text
+assert value_text_products == 'Products'
+
+# check correct url
+url = 'https://www.saucedemo.com/inventory.html'
+get_url = browser.current_url
+assert url == get_url
+print('super')
 
 time.sleep(5)
 browser.close()
