872be5fd9e28dbad27881d0af3ec9538e8deb129
Stepan Kazakov <s.kazakov.1979@gmail.com>
2023-05-26 23:30:08 +0700

    smoke test вщту

--- Selenium/Smoke.py
+++ Selenium/Smoke.py
@@ -1,5 +1,6 @@
 import time
-from random import choice
+import names
+from random import choice, randrange
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
@@ -14,18 +15,16 @@ multipassword = 'secret_sauce'
 browser.get(base_url)
 browser.maximize_window()
 
-# input random username
+# 1 step: input random username, password and enter
 user_name = browser.find_element(by=By.XPATH, value='//*[@id="user-name"]')
 user_name.send_keys(choice(login))
-
-# input password and enter
 password = browser.find_element(by=By.XPATH, value='//*[@id="password"]')
 password.send_keys(multipassword)
-time.sleep(2)
+time.sleep(3)
 password.send_keys(Keys.RETURN)
 print('1 step: login successful.')
 
-# select and add to cart Fleece jacket & Backpack, and remember prices
+# 2 step: select and add to cart Fleece jacket & Backpack, and remember prices
 product_1 = browser.find_element(by=By.XPATH, value='//*[@id="item_5_title_link"]/div')
 value_product_1 = product_1.text
 price_1 = browser.find_element(by=By.XPATH, value='//*[@id="inventory_container"]/div/div[4]/div[2]/div[2]/div')
@@ -39,27 +38,61 @@ value_price_2 = price_2.text
 add_cart_2 = browser.find_element(by=By.XPATH, value='//button[@id="add-to-cart-sauce-labs-backpack"]')
 add_cart_2.click()
 total_price = float(value_price_1.lstrip('$')) + float(value_price_2.lstrip('$'))
-time.sleep(2)
+time.sleep(3)
 print('2 step: choose products successful\n- product 1: ' + value_product_1 + ', price: ' + value_price_1 + '\n- product 2: ' + value_product_2 + ', price: ' + value_price_2 + '\n-- total price: $' + str(total_price))
 
-# go to cart check the products and checkout
+# 3 step: go to cart check the products and checkout
 cart_icon = browser.find_element(by=By.XPATH, value='//div[@id="shopping_cart_container"]')
 cart_icon.click()
 cart_product_1 = browser.find_element(by=By.XPATH, value='//*[@id="item_5_title_link"]/div')
 value_cart_product_1 = cart_product_1.text
+assert value_cart_product_1 == value_product_1
 cart_price_1 = browser.find_element(by=By.XPATH, value='//*[@id="cart_contents_container"]/div/div[1]/div[3]/div[2]/div[2]/div')
 value_cart_price_1 = cart_price_1.text
 assert value_price_1 == value_cart_price_1
 cart_product_2 = browser.find_element(by=By.XPATH, value='//*[@id="item_4_title_link"]/div')
 value_cart_product_2 = cart_product_2.text
+assert value_cart_product_2 == value_product_2
 cart_price_2 = browser.find_element(by=By.XPATH, value='//*[@id="cart_contents_container"]/div/div[1]/div[4]/div[2]/div[2]/div')
 value_cart_price_2 = cart_price_2.text
 assert value_price_2 == value_cart_price_2
-time.sleep(2)
+time.sleep(3)
 checkout_button = browser.find_element(by=By.XPATH, value='//button[@id="checkout"]')
 checkout_button.click()
 print('3 step: go to cart successful')
 
-# fill the personal information form
+# 4 step: fill the personal information form
+first_name = browser.find_element(by=By.XPATH, value='//input[@id="first-name"]')
+first_name.send_keys(names.get_first_name())
+last_name = browser.find_element(by=By.XPATH, value='//input[@id="last-name"]')
+last_name.send_keys(names.get_last_name())
+zip_code = browser.find_element(by=By.XPATH, value='//input[@id="postal-code"]')
+zip_code.send_keys(randrange(100000, 1000000))
+time.sleep(3)
+continue_button = browser.find_element(by=By.XPATH, value='//input[@id="continue"]')
+continue_button.click()
+print('4 step: input personal information successful')
+
+# 5 step: check order overview and finish shopping!
+order_product_1 = browser.find_element(by=By.XPATH, value='//*[@id="item_5_title_link"]/div')
+value_order_product_1 = order_product_1.text
+assert value_order_product_1 == value_cart_product_1
+order_price_1 = browser.find_element(by=By.XPATH, value='//*[@id="checkout_summary_container"]/div/div[1]/div[3]/div[2]/div[2]/div')
+value_order_price_1 = order_price_1.text
+assert value_order_price_1 == value_cart_price_1
+order_product_2 = browser.find_element(by=By.XPATH, value='//*[@id="item_4_title_link"]/div')
+value_order_product_2 = order_product_2.text
+assert value_order_product_2 == value_cart_product_2
+order_price_2 = browser.find_element(by=By.XPATH, value='//*[@id="checkout_summary_container"]/div/div[1]/div[4]/div[2]/div[2]/div')
+value_order_price_2 = order_price_2.text
+assert value_order_price_2 == value_cart_price_2
+order_total_price = browser.find_element(by=By.XPATH, value='//*[@id="checkout_summary_container"]/div/div[2]/div[6]')
+value_order_total_price = order_total_price.text
+item_total = float(value_order_total_price[13:])
+assert item_total == total_price
+time.sleep(3)
+checkout_button = browser.find_element(by=By.XPATH, value='//button[@id="finish"]')
+checkout_button.click()
+print('5 step: checkout successful, finish!')
 
 browser.close()
