9d7244ca83fb0e1e3b537b82f2fc9a84e5da5d60
StepanKazakov <s.kazakov.1979@gmail.com>
2023-05-29 00:11:11 +0700

    expectations MAC

--- SeleniumMAC/exceptions.py
+++ SeleniumMAC/exceptions.py
@@ -1,25 +1,36 @@
 import time
-from datetime import datetime
 from selenium import webdriver
-from selenium.common import NoSuchElementException
-from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 base_url = 'https://demoqa.com/dynamic-properties'
 browser = webdriver.Firefox()
 browser.get(base_url)
-browser.maximize_window()
+# browser.maximize_window()
 
-try:
-    visible_button = browser.find_element(By.XPATH, value="//button[@id='visibleAfter']")
-    visible_button.click()
+# явные ожидания устанавливаются индивидуально для каждого элемента и срабатывают только когда элемент станет доступным для действия
+visible_button = WebDriverWait(browser, 30).until(EC.element_to_be_clickable("//button[@id='visibleAfter']"))
+visible_button.click()
+print('ok')
 
-except NoSuchElementException as exception:
-    print('NoSuchElementException')
-    browser.refresh()
-    time.sleep(6)
-    visible_button = browser.find_element(By.XPATH, value="//button[@id='visibleAfter']")
-    visible_button.click()
-    print('Click visible button')
+# неявные ожидания будут срабатывать для каждой операции теста и ждать до 10 секунд появления элементов в DOM
+# browser.implicitly_wait(10)
+# visible_button = browser.find_element(By.XPATH, value="//button[@id='visibleAfter']")
+# visible_button.click()
+# print('ok')
+
+# обработка исключений
+# try:
+#     visible_button = browser.find_element(By.XPATH, value="//button[@id='visibleAfter']")
+#     visible_button.click()
+#
+# except NoSuchElementException as exception:
+#     print('NoSuchElementException')
+#     browser.refresh()
+#     time.sleep(6)
+#     visible_button = browser.find_element(By.XPATH, value="//button[@id='visibleAfter']")
+#     visible_button.click()
+#     print('Click visible button')
 
 browser.close()
